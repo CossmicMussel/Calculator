0185610e8721aad7a7125b81caa6eaf4192d74af
CossmicMussel <62438689+CossmicMussel@users.noreply.github.com>
2024-07-28 13:56:28 -0500

    Update Calulator.py

--- Calulator.py
+++ Calulator.py
@@ -1,8 +1,9 @@
-print("This is v3 calculator script (addition, subtraction, and Multiplication)")
+print("This is v4 calculator script (All operations)")
 print("please choose an operation: ")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
+print("4. Division")
 choice = input("Enter a number selection --> ")
 
 num1 = int(input("Enter your first number --> "))
@@ -13,5 +14,7 @@ elif choice == "2":
     print(f"{num1} - {num2} = {num1-num2}")
 elif choice == "3":
     print(f"{num1} * {num2} = {num1 * num2}")
+elif choice == "4":
+    print(f"{num1} * {num2} = {num1 / num2}")
 else:
     print("No valid option selected.")
