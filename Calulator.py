d8de7a9d88dcc57a02181f6db3d6b35dbfd268f0
CossmicMussel <62438689+CossmicMussel@users.noreply.github.com>
2024-07-28 13:42:00 -0500

    Subtraction added
    Subtraction operation added

--- Calulator.py
+++ Calulator.py
@@ -1,4 +1,14 @@
-print("This is v1 calculator script (Addition only)")
+print("This is v1 calculator script (addition and subtraction only)")
+print("please choose an operation: ")
+print("1. Addition")
+print("2. Subtraction")
+choice = input("Enter a number selection --> ")
+
 num1 = int(input("Enter your first number --> "))
 num2 = int(input("Enter your second number --> "))
-print(f"{num1} + {num2} = {num1+num2}")
+if choice == "1":
+    print(f"{num1} + {num2} = {num1+num2}")
+elif choice == "2":
+    print(f"{num1} - {num2} = {num1-num2}")
+else:
+    print("No valid option selected.")
